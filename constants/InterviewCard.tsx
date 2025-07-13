d8392490b7ce57436f7e4b6b1690a7c19f8c9511
Ganesh K <ganesh150eee@gmail.com>
2025-07-14 00:50:38 +0530

    Updated Home Page

--- constants/InterviewCard.tsx
+++ constants/InterviewCard.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import dayjs from 'dayjs'
 import { getRandomInterviewCover } from "@/public/utils"
 import Image from "next/image"
+import {Button} from "@/Components/ui/button";
+import Link from "next/link";
 
 const InterviewCard = ({
                            interviewId,
@@ -43,6 +45,23 @@ const InterviewCard = ({
                         <Image src="/calendar.svg" alt="calendar" width={22} height={22} />
                         <p>{formattedDate}</p>
                     </div>
+
+                    <div className="flex flex-row gap-2 items-center">
+                        <Image src="/star.svg" alt="star" width={22} height={22}/>
+                        <p>{feedback?.totalScore || '---'}/100</p>
+                    </div>
+
+                    <p className="line-clamp-2 mt-5">
+                        {feedback?.finalAssessment || "You haven't taken the interview yet. Take it now to improve your skills"}
+                    </p>
+                </div>
+
+                <div className="flex flex-row justify-between ">
+                    <p>Tech Icons</p>
+
+                    <Button className="btn-primary">
+                        <Link href={feedback?`/interview/${interviewId}/feedback` : `/interview/${interviewId}`}>{feedback? 'Check Feedback' : 'View Interview'}</Link>
+                    </Button>
 
                 </div>
             </div>
