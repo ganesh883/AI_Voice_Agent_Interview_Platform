635dea7e5cedd5237eeade247f448e185237bd58
Ganesh K <ganesh150eee@gmail.com>
2025-07-15 02:38:47 +0530

    Validate User Sign Up

---
+++ lib/actions/auth.action.ts
@@ -0,0 +1,23 @@
+'use server';
+
+export async function signUp(params : SignUpParams){
+    const { uid, name, email } = params;
+
+    try {
+
+    } catch(e : any) {
+        console.log('Error creating a User', e);
+
+        if(e.code === 'auth/email-already-exists'){
+            return {
+                success : false,
+                message : 'This email already exists'
+            }
+        }
+
+        return{
+            success : false,
+            message : 'Failed to Create an Account'
+        }
+    }
+}
