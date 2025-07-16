ee12c64e75902aa1ef3d26256214bf35185df12e
Ganesh K <ganesh150eee@gmail.com>
2025-07-17 00:32:07 +0530

    Updated Sign Up and Sign In Functionality

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,9 +1,23 @@
 'use server';
 
+import {db} from "@/Firebase/admin";
+
 export async function signUp(params : SignUpParams){
     const { uid, name, email } = params;
 
     try {
+
+        const userRecord = await db.collection('users').doc(uid).get();
+
+        if(userRecord.exists){
+            return{
+                success: false,
+                message: 'User already exists!.Please Sign In instead.'
+            }
+        }
+
+        await db.collection('users').doc(uid).set({name, email});
+
 
     } catch(e : any) {
         console.log('Error creating a User', e);
