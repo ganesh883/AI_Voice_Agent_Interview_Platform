f5aa758adb23d35d408eaa107791ffa1e70079bc
Ganesh K <ganesh150eee@gmail.com>
2025-08-26 14:53:52 +0530

    Updated Error

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -7,7 +7,7 @@ const Page = async () => {
     const user = await getCurrentUser();
     return (
        <>
-                <h3 >Interview Generation</h3>
+                <h3>Interview Generation</h3>
                 <Agent   userName={user?.name} userId={user?.id} type="generate"/>
        </>
     )
